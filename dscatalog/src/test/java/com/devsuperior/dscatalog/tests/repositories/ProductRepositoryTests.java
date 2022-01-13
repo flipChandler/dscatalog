e069acbef398d3f0f30fda9b5d5d1201bacc6ddf
flipChandler <felipe.santos11@hotmail.com>
2022-01-13 13:57:27 -0300

    product repository test - findProductsWithCategories - pageRequest

--- dscatalog/src/test/java/com/devsuperior/dscatalog/tests/repositories/ProductRepositoryTests.java
+++ dscatalog/src/test/java/com/devsuperior/dscatalog/tests/repositories/ProductRepositoryTests.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -14,7 +16,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repository.ProductRepository;
 import com.devsuperior.dscatalog.tests.factory.ProductFactory;
@@ -27,12 +32,71 @@ public class ProductRepositoryTests {
 	private long existingId;
 	private long nonExistingId;
 	private long countTotalProducts;
+	private long countPCGamerProducts;
+	private PageRequest pageRequest;
+	private List<Category> categories = new ArrayList<>();
 	
 	@BeforeEach
 	public void setup() throws Exception {
 		existingId = 1L;
 		nonExistingId = 1000;
 		countTotalProducts = 25L;
+		countPCGamerProducts = 21L; 	// data.sql tem 21 PC GAMER
+		pageRequest = PageRequest.of(0, 10);
+		categories.add(new Category(1L, "Livros"));		 // 1 produto com essa categoria
+		categories.add(new Category(2L, "Eletrônicos")); // 2 produtos com essa categoria
+	}
+	
+	@Test
+	public void findProductsWithCategoriesShouldReturnProducts_whenCategoriesExist() {
+		String name = "";
+		
+		Page<Product> result = productRepository.findProductsWithCategories(categories, name, pageRequest);
+		
+		assertFalse(result.isEmpty());
+		assertEquals(3, result.getTotalElements());
+	}
+	
+	@Test
+	public void findProductsWithCategoriesShouldReturnAnEmptyList_whenCategoriesIsIncorrect() {
+		String name = "";
+		categories.clear();
+		categories.add(new Category(4L, "Garden"));
+		
+		Page<Product> result = productRepository.findProductsWithCategories(categories, name, pageRequest);
+		
+		assertTrue(result.isEmpty());
+		assertEquals(0, result.getTotalElements());
+	}
+	
+	@Test
+	public void findProductsWithCategoriesShouldReturnProducts_whenNameIsEmpty() {
+		String name = "";
+		
+		Page<Product> result = productRepository.findProductsWithCategories(null, name, pageRequest);
+		
+		assertFalse(result.isEmpty());
+		assertEquals(countTotalProducts, result.getTotalElements());
+	}
+	
+	@Test
+	public void findProductsWithCategoriesShouldReturnProducts_whenNameExists() {
+		String name = "PC Gamer";
+		
+		Page<Product> result = productRepository.findProductsWithCategories(null, name, pageRequest);
+		
+		assertFalse(result.isEmpty());
+		assertEquals(countPCGamerProducts, result.getTotalElements());
+	}
+	
+	@Test
+	public void findProductsWithCategoriesShouldReturnProducts_whenNameExistsIgnoringCase() {
+		String name = "pc gAMer";
+		
+		Page<Product> result = productRepository.findProductsWithCategories(null, name, pageRequest);
+		
+		assertFalse(result.isEmpty());
+		assertEquals(countPCGamerProducts, result.getTotalElements());
 	}
 	
 	@Test
