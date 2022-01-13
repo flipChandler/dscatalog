cd6685770e1f3254494cfe0bfb58f6319e6eaf4e
flipChandler <felipe.santos11@hotmail.com>
2022-01-13 11:53:48 -0300

    product repository test - save - id auto increment

--- dscatalog/src/test/java/com/devsuperior/dscatalog/tests/repositories/ProductRepositoryTests.java
+++ dscatalog/src/test/java/com/devsuperior/dscatalog/tests/repositories/ProductRepositoryTests.java
@@ -1,7 +1,11 @@
 package com.devsuperior.dscatalog.tests.repositories;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
@@ -13,6 +17,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repository.ProductRepository;
+import com.devsuperior.dscatalog.tests.factory.ProductFactory;
 
 @DataJpaTest							// teste de repository precisa somente dessa annotation
 public class ProductRepositoryTests {
@@ -21,11 +26,27 @@ public class ProductRepositoryTests {
 	private ProductRepository productRepository;	
 	private long existingId;
 	private long nonExistingId;
+	private long countTotalProducts;
 	
 	@BeforeEach
 	public void setup() throws Exception {
 		existingId = 1L;
 		nonExistingId = 1000;
+		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void saveShouldPersistWithAutoincrement_whenIdIsNull() {
+		Product product = ProductFactory.createProduct();
+		product.setId(null);
+		
+		product = productRepository.save(product);
+		Optional<Product> result = productRepository.findById(product.getId());
+		
+		assertNotNull(product.getId());
+		assertEquals(countTotalProducts + 1L, product.getId());
+		assertTrue(result.isPresent());
+		assertSame(result.get(), product);							// se é o mesmo objeto (cache da JPA não precisou ir ao banco de dados)
 	}
 	
 	@Test
