d762853e5e025921598fb8cff25077d7d22684c5
flipChandler <felipe.santos11@hotmail.com>
2022-01-16 21:43:48 -0300

    category resource tests - find by id - exception

--- dscatalog/src/test/java/com/devsuperior/dscatalog/tests/web/CategoryResourceTests.java
+++ dscatalog/src/test/java/com/devsuperior/dscatalog/tests/web/CategoryResourceTests.java
@@ -1,13 +1,17 @@
 package com.devsuperior.dscatalog.tests.web;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.json.JacksonJsonParser;
@@ -15,6 +19,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.LinkedMultiValueMap;
@@ -22,6 +27,7 @@ import org.springframework.util.MultiValueMap;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscatalog.tests.factory.CategoryFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -64,6 +70,29 @@ public class CategoryResourceTests {
 		existingCategoryDTO = CategoryFactory.createCategoryDTO(existingId);
 		page = new PageImpl<>(List.of(existingCategoryDTO));
 	}
+	
+	@Test
+	public void findById_ShouldReturnCategory_whenIdExists() throws Exception {
+		when(categoryService.findById(existingId)).thenReturn(existingCategoryDTO);
+		
+		ResultActions result = mockMvc.perform(get("/categories/{id}", existingId)
+				.accept(MediaType.APPLICATION_JSON));
+				
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.id").value(existingId));
+		result.andExpect(jsonPath("$.name").value(existingCategoryDTO.getName()));
+	}
+	
+	@Test
+	public void findById_ShouldThrowResourceNotFoundException_whenIdDoesNotExist() throws Exception {
+		when(categoryService.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+		
+		ResultActions result = mockMvc.perform(get("/categories/{id}", nonExistingId)
+				.accept(MediaType.APPLICATION_JSON));
+				
+		result.andExpect(status().isNotFound());		
+	}	
 		
 	private String obtainAccessToken(String username, String password) throws Exception {
 		 
